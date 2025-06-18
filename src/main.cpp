5b7b9c7c5386b0e4457adba77ee165be58a1b7fb
benjag22 <benjagonzalez2022@udec.cl>
2025-06-18 15:39:50 -0400

    feat: add main execution for a cluster and 3 iterations

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include "k-means.h"
 
-int main() {return 0;}
+int main() {
+    std::cout << "=== Ejercicio 1 ===" << std::endl;
+
+    Kmeans kmeans(3);
+
+    kmeans.addPoint('A', 2, 10);
+    kmeans.addPoint('B', 2, 5);
+    kmeans.addPoint('C', 8, 4);
+    kmeans.addPoint('D', 5, 8);
+    kmeans.addPoint('E', 7, 5);
+    kmeans.addPoint('F', 6, 4);
+    kmeans.addPoint('G', 1, 2);
+    kmeans.addPoint('H', 4, 9);
+
+    kmeans.printEuclideanMatrix();
+
+    std::vector<Centroid> initialCentroids1 = {
+            Centroid(2, 10, 'A'), // Cluster 0
+            Centroid(5, 8, 'D'),  // Cluster 1
+            Centroid(1, 2, 'G')   // Cluster 2
+    };
+    kmeans.setInitialCentroids(initialCentroids1);
+
+    kmeans.run(3);
+    return 0;
+}
